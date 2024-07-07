f212e281598d5699f70c3867fa47cbbeecf3e1fd
harshnayakal <harshnayakal@gmail.com>
2024-07-07 21:48:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const counter = document.getElementById('counter');
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+const resetButton = document.getElementById('reset');
+
+let count = 0;
+
+function updateCounter() {
+    counter.textContent = count;
+}
+
+incrementButton.addEventListener('click', () => {
+    count++;
+    updateCounter();
+});
+
+decrementButton.addEventListener('click', () => {
+    count--;
+    updateCounter();
+});
+
+resetButton.addEventListener('click', () => {
+    count = 0;
+    updateCounter();
+});
